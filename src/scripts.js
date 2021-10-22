17495ba2d676d9cadb7c9ea7114ff85a76cc7f1d
Jhonatan Becerra <jbecerrap95@gmail.com>
2021-10-22 07:35:47 -0500

    Arreglando el funcionamiento general y listo para el push en nueva maquina

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,5 @@
 const player = document.getElementById('player');
 
-<<<<<<< HEAD
-
 //Agregar y remover clase de salto
 document.addEventListener('click', function () {
     player.classList.add('player_jump');
@@ -9,14 +7,3 @@ document.addEventListener('click', function () {
 player.addEventListener('animationend', () => {
     player.classList.remove('player_jump');
 });
-
-  
-=======
-document.addEventListener('click', function () {
-    player.classList.add('player_jump');
-})
-
-player.addEventListener('animationend', () => {
-    player.classList.remove('player_jump');
-  });
->>>>>>> b9bc6d23528fc140fb4bfb4b989cd468323bac40
